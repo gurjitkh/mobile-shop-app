e2f451e1a59e720939a5db6fc476e96087056441
Gurjit Singh <gurjitsingh1912@gmail.com>
2020-04-26 21:45:13 -0400

    Updates

---
+++ precache-manifest.0e52ae1f1379816c33605867a259e102.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03371839c483e4dc0fa1857334af93dc",
+    "url": "/mobile-shop-app/index.html"
+  },
+  {
+    "revision": "6733553d51006d3bb5f2",
+    "url": "/mobile-shop-app/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "1e638376d7e8c495b875",
+    "url": "/mobile-shop-app/static/css/main.4b88d8db.chunk.css"
+  },
+  {
+    "revision": "6733553d51006d3bb5f2",
+    "url": "/mobile-shop-app/static/js/2.89b3b0e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mobile-shop-app/static/js/2.89b3b0e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e638376d7e8c495b875",
+    "url": "/mobile-shop-app/static/js/main.fdb46a70.chunk.js"
+  },
+  {
+    "revision": "0fe246b81b8489d6d29c",
+    "url": "/mobile-shop-app/static/js/runtime-main.348e0ad0.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/mobile-shop-app/static/media/logo.9f7e506d.svg"
+  }
+]);
